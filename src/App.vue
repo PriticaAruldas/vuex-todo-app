ec79bcec1b7780248199723d0be70d4cd1ea58a2
PriticaAruldas <pritica@metapercept.com>
2020-05-13 19:15:36 +0530

    vuex todo app

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <!-- <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div> -->
+    <AddTodos/>
+    <Todos/>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+ 
+import Todos from './components/todos.vue'
+import AddTodos from './components/AddTodos.vue'
+ 
+export default {
+  name: "app",
+  components: {
+    Todos,
+    AddTodos
+  } 
+  
+}
+</script>
+
+<style lang="scss">
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="home">
+    <!-- <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <div class="container">
+      <Todos/>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Todos from '@/components/todos.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Todos
+  }
+}
+</script>
+
+<style  lang="scss">
+body{
+  font-family:'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+  line-height: 1.6;
+  background:rgb(191, 238, 191); 
+}
+.container{
+  max-width:1100px;
+  margin: auto;
+  overflow:auto;
+  padding: 0 2rem;
+}
+</style>
